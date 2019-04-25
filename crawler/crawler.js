fc2235b929156f6f16cac122402dcbed38623756
Cássio Santos <sss.cassio@gmail.com>
2019-04-25 18:31:47 -0300

    :rocket: Trying stuff

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,9 +1,9 @@
-const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-const users_selector = require("./selectors/users.js");
-const search_selector = require("./selectors/search.js");
-const feed_selector = require("./selectors/feed.js");
-const media_selector = require("./selectors/media.js");
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const users_selector = require('./selectors/users.js');
+const search_selector = require('./selectors/search.js');
+const feed_selector = require('./selectors/feed.js');
+const media_selector = require('./selectors/media.js');
 
 class Crawler {
   constructor() {
@@ -12,13 +12,15 @@ class Crawler {
 
   async initialize(pathUrl) {
     if (!this.page) {
-      // Initialize Browser
-      this.browser = await puppeteer.launch();
+      // Initialize Browser: Issue: https://github.com/GoogleChrome/puppeteer/issues/758#issuecomment-328906039
+      this.browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
       // Go to user profile
       this.page = await this.browser.newPage();
       await this.page.setViewport({ width: 1080, height: 720 });
       await this.page.goto(`${this.baseUrl}${pathUrl}`, {
-        waitUntil: "networkidle0"
+        waitUntil: 'networkidle0'
       });
     }
   }
@@ -29,11 +31,11 @@ class Crawler {
     const $ = await cheerio.load(bodyHTML);
     const username = await $(users_selector.USERNAME).text();
     const full_name = await $(users_selector.FULL_NAME).text();
-    const profile_picture = await $(users_selector.PROFILE_PICTURE).attr("src");
+    const profile_picture = await $(users_selector.PROFILE_PICTURE).attr('src');
     const bio = await $(users_selector.BIO).text();
     const website = await $(users_selector.WEBSITE).text();
     const media = await $(users_selector.MEDIA_COUNT).text();
-    const followed_by = await $(users_selector.FOLLOWED_BY_COUNT).attr("title");
+    const followed_by = await $(users_selector.FOLLOWED_BY_COUNT).attr('title');
     const follows = await $(users_selector.FOLLOWS_COUNT).text();
 
     return {
@@ -43,20 +45,20 @@ class Crawler {
       bio,
       website,
       counts: {
-        media: parseInt(media.replace(/,/g, "")),
-        follows: parseInt(follows.replace(/,/g, "")),
-        followed_by: parseInt(followed_by.replace(/,/g, ""))
+        media: parseInt(media.replace(/,/g, '')),
+        follows: parseInt(follows.replace(/,/g, '')),
+        followed_by: parseInt(followed_by.replace(/,/g, ''))
       }
     };
   }
 
   async searchUser(query) {
     await this.initialize(`search`);
-    await this.page.screenshot({ path: "./crawler/screenshots/search.png" });
+    await this.page.screenshot({ path: './crawler/screenshots/search.png' });
     await this.page.type(search_selector.SEARCH_INPUT, query);
-    await this.page.waitForNavigation({ waitUntil: "networkidle0" });
+    await this.page.waitForNavigation({ waitUntil: 'networkidle0' });
     await this.page.screenshot({
-      path: "./crawler/screenshots/search_typed.png"
+      path: './crawler/screenshots/search_typed.png'
     });
     await this.page.waitForSelector(search_selector.SEARCH_LIST);
 
@@ -67,7 +69,7 @@ class Crawler {
       .map((index, element) => ({
         profile_picture: $(element)
           .find(search_selector.PROFILE_PICTURE)
-          .attr("src"),
+          .attr('src'),
         full_name: $(element)
           .find(search_selector.FULL_NAME)
           .text(),
@@ -76,12 +78,12 @@ class Crawler {
           .text()
       }))
       .get()
-      .filter(value => value.username.charAt(0) != "#");
+      .filter(value => value.username.charAt(0) != '#');
   }
 
   async scrapUserFeed(username, count = 100, scrollDelay = 1000) {
     await this.initialize(`${username}`);
-    await this.page.screenshot({ path: "./crawler/screenshots/feed.png" });
+    await this.page.screenshot({ path: './crawler/screenshots/feed.png' });
     const bodyHTML = await this.page.evaluate(() => document.body.innerHTML);
     const $ = await cheerio.load(bodyHTML);
 
@@ -90,17 +92,17 @@ class Crawler {
       .map((index, element) => {
         const media_id = $(element)
           .find(feed_selector.FEED_LINK)
-          .attr("href")
+          .attr('href')
           .slice(1);
         return {
           link: `${this.baseUrl}${media_id}`,
           media_id,
           picture: $(element)
             .find(feed_selector.FEED_PICTURE)
-            .attr("src"),
+            .attr('src'),
           picture_alt: $(element)
             .find(feed_selector.FEED_PICTURE)
-            .attr("alt")
+            .attr('alt')
         };
       })
       .get();
@@ -108,7 +110,7 @@ class Crawler {
 
   async scrapMediaInfo(mediaId) {
     await this.initialize(`p/${mediaId}`);
-    await this.page.screenshot({ path: "./crawler/screenshots/media.png" });
+    await this.page.screenshot({ path: './crawler/screenshots/media.png' });
     await this.page.waitFor(1000);
     await loadMoreComments(this.page);
 
@@ -159,79 +161,79 @@ class Crawler {
             .html() == null
         )
       )
-        return "image";
-      if (!($(media_selector.IMAGES).html() == null)) return "images";
-      if (!($(media_selector.VIDEO).html() == null)) return "video";
+        return 'image';
+      if (!($(media_selector.IMAGES).html() == null)) return 'images';
+      if (!($(media_selector.VIDEO).html() == null)) return 'video';
     })($);
 
     // Get media Images or videos
     let videos = [],
       images = [];
     switch (type) {
-      case "image":
+      case 'image':
         images.push({
           height: $(media_selector.LEFT_BOX)
             .find(media_selector.IMAGE)
-            .attr("sizes")
-            .replace("px", ""),
+            .attr('sizes')
+            .replace('px', ''),
           width: $(media_selector.LEFT_BOX)
             .find(media_selector.IMAGE)
-            .attr("sizes")
-            .replace("px", ""),
+            .attr('sizes')
+            .replace('px', ''),
           image_alt: $(media_selector.LEFT_BOX)
             .find(media_selector.IMAGE)
-            .attr("alt"),
+            .attr('alt'),
           url: $(media_selector.LEFT_BOX)
             .find(media_selector.IMAGE)
-            .attr("src")
+            .attr('src')
         });
         break;
-      case "images":
+      case 'images':
         $(media_selector.IMAGES).each((index, element) => {
           images.push({
             height: $(element)
               .find(media_selector.IMAGE_FROM_IMAGES)
-              .attr("sizes")
-              .replace("px", ""),
+              .attr('sizes')
+              .replace('px', ''),
             width: $(element)
               .find(media_selector.IMAGE_FROM_IMAGES)
-              .attr("sizes")
-              .replace("px", ""),
+              .attr('sizes')
+              .replace('px', ''),
             image_alt: $(element)
               .find(media_selector.IMAGE_FROM_IMAGES)
-              .attr("alt"),
+              .attr('alt'),
             url: $(element)
               .find(media_selector.IMAGE_FROM_IMAGES)
-              .attr("src")
+              .attr('src')
           });
         });
-        type = "image";
+        type = 'image';
         break;
-      case "video":
+      case 'video':
         videos.push({
-          thumbnail: $(media_selector.VIDEO).attr("poster"),
-          url: $(media_selector.VIDEO).attr("src")
+          thumbnail: $(media_selector.VIDEO).attr('poster'),
+          url: $(media_selector.VIDEO).attr('src')
         });
         break;
-      case "videos": // TODO:
+      case 'videos': // TODO:
     }
     const username = $(media_selector.MEDIA_HEADER)
       .find(media_selector.CREATOR_USERNAME)
       .text();
     const profile_picture = $(media_selector.MEDIA_HEADER)
       .find(media_selector.CREATOR_PROFILE_PICTURE)
-      .attr("src");
+      .attr('src');
     const text = $(media_selector.COMMENTS_BOX)
       .find(media_selector.CAPTION_TEXT)
       .text();
-    const created_time = $(media_selector.CREATION_TIME).attr("datetime");
+    const created_time = $(media_selector.CREATION_TIME).attr('datetime');
     const likes = parseInt(
-      (type == "image"
+      (type == 'image'
         ? $(media_selector.LIKES_COUNT_IMAGE)
         : $(media_selector.LIKES_COUNT_VIDEO)
       )
         .text()
-        .replace(/,/g, "")
+        .replace(/,/g, '')
     );
     const location_name = $(media_selector.MEDIA_HEADER)
       .find(media_selector.LOCATION_NAME)
